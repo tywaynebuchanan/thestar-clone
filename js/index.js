bc2c8179910a5f8819619027577e2e6abea2f3ac
Tywayne <tywaynebuchanan@gmail.com>
2021-10-31 22:46:36 -0500

    feature: added hero section

---
+++ js/index.js
@@ -0,0 +1,93 @@
+document.getElementById('root').innerHTML = `
+
+<section class="social-media">
+    <div class="social-content">
+        <p class="social-text">Follow us on</p>
+        <a href="#" class="social-media-links"><ion-icon name="logo-facebook" class="icon-social"></ion-icon></a>
+        <a href="#" class="social-media-links"><ion-icon name="logo-twitter" class="icon-social"></ion-icon></a>
+        <a href="#" class="social-media-links"><ion-icon name="logo-instagram" class="icon-social"></ion-icon></a>
+        <a href="#" class="social-media-links"><ion-icon name="logo-youtube" class="icon-social"></ion-icon></a>
+    </div>
+</section>
+<header class="header">
+    <a href="#">
+        <img src="/img/logo.png" alt="Logo" class="logo">
+    </a>
+    <nav class="main-nav">
+        <ul class="main-nav-list">
+            <li><a href="#" class="nav-links">Home</a></li>
+            <li><a href="#" class="nav-links">News</a></li>
+            <li><a href="#" class="nav-links">Sports</a></li>
+            <li><a href="#" class="nav-links">Entertainment</a></li>
+            <li><a href="#" class="nav-links">Tell me Paster</a></li>
+            <li><a href="#" class="nav-links">Vidoes</a></li>
+            <li><a href="#" class="nav-links">Photos</a></li>
+            <li><a href="#" class="nav-links">More</a> </li>
+            <li><a href="#" class="nav-links"></a></li>
+        </ul>
+    </nav>
+</header>
+
+<section class="section-hero">
+    <div class="container grid g-3-col mb-1">
+        <div class="main-news">
+            <h1 class="heading-primary mb">
+                Get the latest News 
+            </h1>
+            <h2 class="heading-secondary">
+                Jamaica's Leading News Portal
+            </h2>
+        </div>
+        <div class="secondary-news">
+            <div class="card-content">
+                <h1 class="heading-secondary mb">More In store</h1>
+                <p class="news-story-text mb">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita autem amet explicabo adipisci non, provident ipsam eligendi voluptatem. Recusandae molestiae delectus soluta qui quos eius dolor aliquid modi impedit.
+                </p>
+                <a href="#" class="cta-btn">Read More</a>
+            </div>
+
+        </div>
+        <div class="other-news">
+            <div class="card-content">
+                <h1 class="heading-secondary mb">More In store</h1>
+                <p class="news-story-text mb">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita autem amet explicabo adipisci non, provident ipsam eligendi voluptatem. Recusandae molestiae delectus soluta qui quos eius dolor aliquid modi impedit.
+                </p>
+                <a href="#" class="cta-btn">Read More</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container grid g-3-col">
+        <div class="main-news">
+            <!-- <h1 class="heading-primary mb">
+                Get the latest News 
+            </h1>
+            <h2 class="heading-secondary">
+                Jamaica's Leading News Portal
+            </h2> -->
+        </div>
+        <div class="secondary-news">
+            <div class="card-content">
+                <h1 class="heading-secondary mb text-light">More In store</h1>
+                <p class="news-story-text mb text-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita autem amet explicabo adipisci non, provident ipsam eligendi voluptatem. Recusandae molestiae delectus soluta qui quos eius dolor aliquid modi impedit.
+                </p>
+                <a href="#" class="cta-btn">Read More</a>
+            </div>
+        </div>
+        <div class="other-news">
+            <div class="card-content">
+                <h1 class="heading-secondary mb text-light">More In store</h1>
+                <p class="news-story-text mb text-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus expedita autem amet explicabo adipisci non, provident ipsam eligendi voluptatem. Recusandae molestiae delectus soluta qui quos eius dolor aliquid modi impedit.
+                </p>
+                <a href="#" class="cta-btn">Read More</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+`;
